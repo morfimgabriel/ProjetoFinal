7af4cc06129aca7beea81ef0bb6c924f5d20f871
Gabriel Morfim <morfimgabriel@hotmail.com>
2018-06-18 10:51:30 -0300

    segundo commit

--- app/src/main/java/gabriel/br/com/projetofinal/MainActivity.java
+++ app/src/main/java/gabriel/br/com/projetofinal/MainActivity.java
@@ -2,6 +2,8 @@ package gabriel.br.com.projetofinal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.MultiAutoCompleteTextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, COUNTRIES);
+        MultiAutoCompleteTextView textView = findViewById(R.id.edit);
+        textView.setAdapter(adapter);
+        textView.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
     }
+
+    private static final String[] COUNTRIES = new String[] {
+            "Belgium", "France", "Italy", "Germany", "Spain"
+    };
 }
+
+
