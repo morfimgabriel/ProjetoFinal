5e345edf3d57a20377924ac603476037aace12b7
morfimgabriel <morfimgabriel@hotmail.com>
2018-06-20 11:00:41 -0300

    commitando telas e styles

--- app/src/main/java/gabriel/br/com/projetofinal/CadastrarUsuarioActivity.java
+++ app/src/main/java/gabriel/br/com/projetofinal/CadastrarUsuarioActivity.java
@@ -2,6 +2,7 @@ package gabriel.br.com.projetofinal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class CadastrarUsuarioActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class CadastrarUsuarioActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar_usuario);
+    }
+
+    public void CadastrarUsuario(View view) {
     }
 }
